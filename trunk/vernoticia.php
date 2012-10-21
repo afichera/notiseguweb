a58ac20695cf5e3d8f641c63c022cdf0473b5da9
sabrinamarielcasado@gmail.com <sabrinamarielcasado@gmail.com@7f223ab5-162b-1bb3-6a47-6b799e18ccec>
2012-10-21 17:16:21 +0000

    Los cambios estan en noticia_nueva y noticia_nueva_submit. Ver noticia esta incompleto, no logro hacerlo funcionar. Igual sigo intentando

--- trunk/vernoticia.php
+++ trunk/vernoticia.php
@@ -41,6 +41,7 @@ else
 	for ($i=0; $i<mysql_num_rows($consulta); $i++)
 {
 $fila = mysql_fetch_array($consulta);
+$id=$fila["id"];
 $titulo=$fila["titulo"];
 $fecha=$fila["fecha_hora"];
 $texto=$fila["texto"];
@@ -48,8 +49,40 @@ $autor=$fila["nombre_apellido"];
 echo "<h1>".$titulo."</h1>";
 
 }
-	
- ?> 	
+?>	
+<?php 
+	if(isset($_POST["comentar"])){
+	$var = $_POST["comentar"]; 
+		if ($var == "comentar"){
+			
+		$comentario=$_POST['comentario'];
+		echo $comentario;
+		 	
+		//Averiguo cual es el id del usuario que inicio sesion
+		$nombreUsuario=$sesion->get("usuario");	
+		$conexion=conectarbd("localhost","root","","notiseguweb");
+		$query="SELECT * FROM usuario WHERE nombre_usu='$nombreUsuario'";
+		$consulta2=consulta($query, $conexion);
+			
+		while(isset($consulta2) && $row = mysql_fetch_array($consulta2)) {
+			$idSesion=$row["id"];
+			$nombreSesion=$row["nombre_apellido"];
+		}
+		
+		
+		
+		$conexion=conectarbd("localhost","root","","notiseguweb");
+		$query="INSERT INTO comentario('id', 'texto', 'fecha_hora', 'nota_id', 'usuario_id', 'comentador')
+		VALUES('','lala','NOW()','1','1','lala');";
+		$consulta2=consulta($query, $conexion);
+		
+		if ($consulta2==1){			
+		echo "La operacion se realiz&oacute; con &eacute;xito";						
+		}
+			
+	}
+}
+?>
     </div>
   <div class="grid_6" id="autor" >
 <?php echo $autor;?>
@@ -65,7 +98,11 @@ echo "<h1>".$titulo."</h1>";
     <div class="clear"></div>
     <div class="grid_8" id="form_comentarios" >
     	<h2>Realice un comentario:</h2>
-<?php echo "PONER ACA EL FROMULARIO PARA CARGAR COMENTARIOS";?>
+    	<form name="formulario" action="vernoticia.php" method="post">
+			<input type="text" id="comentario" name="comentario"/>
+    		<input type="submit" id="comentar" name="comentar" value="Comentar"/>
+    	</form>    	
+
   </div> 
       <div class="grid_8" id="comentarios">
       	<h2>Comentarios:</h2>
