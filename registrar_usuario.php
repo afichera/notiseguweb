517ade10bd90f925aceb8fe635b6c305883874cc
alejandrofichera@gmail.com <alejandrofichera@gmail.com@7f223ab5-162b-1bb3-6a47-6b799e18ccec>
2012-11-17 00:46:42 +0000

    Encriptacion.

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -1,59 +1,76 @@
-<?php 
-	session_start();
-	
-	include "conexion.php";
-	
-		
-	$nombre = $_POST['nombre'];
-	$apellido = $_POST['apellido'];
-	$dni = $_POST['dni'];
-	$username = $_POST['username'];
-	$pass = $_POST['clave1'];
-	$tipo_us = $_POST['tipo_usuario'];
-	$estado = $_POST['estado'];
-	
+<?php
+session_start();
 
-	$query='INSERT INTO users (username, roleId, dni, name, surname, password, creationDate, isDeleted) VALUES("'.$username.'",'.$tipo_us.','.$dni.',"'.$nombre.'","'.$apellido.'","'.$pass.'",NOW(),'.$estado.')';
-	
-	mysql_query($query);
+include "conexion.php";
 
-	mysql_close($conexion);
-	
-	switch($_SESSION['rango'])
-	{
-		case ("1"):
-			//echo 'Editor';
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$dni = $_POST['dni'];
+$username = $_POST['username'];
+$pass = $_POST['clave1'];
+$tipo_us = $_POST['tipo_usuario'];
+$estado = $_POST['estado'];
+
+//Modificacion para encriptar el usuario, algo básico y reversible.
+$usuario_encriptado1 = md5($username);
+
+//Modificación para encriptar la contraseña. Varios Niveles
+//Comenzamos con la encriptacion.
+//Encriptacion nivel 1 MD5
+$pass_encriptada1 = md5 ($pass);
+//Encriptacion nivel 1 CRC32
+$pass_encriptada2 = crc32($pass_encriptada1);
+//Encriptacion nivel 2, se agrega un Salt/Clave
+$pass_encriptada3 = crypt($pass_encriptada2, "E5t0EsUnaCl4veSegur4"); 
+//Encriptacion nivel 3 SHA1, se agrega el mismo 
+// Salt/Clave aunque podría ser diferente.
+$pass_encriptada4 = sha1("E5t0EsUnaCl4veSegur4".$pass_encriptada3); 
+
+
+
+$query='INSERT INTO users (username, roleId, dni, name, surname, password, creationDate, isDeleted) 
+VALUES("'.$usuario_encriptado1.'",'.$tipo_us.','.$dni.',"'.$nombre.'","'.$apellido.'","'.$pass_encriptada4.'",NOW(),'.$estado.')';
+
+mysql_query($query);
+
+mysql_close($conexion);
+
+switch($_SESSION['rango'])
+{
+	case ("1"):
+		//echo 'Editor';
 			
-			echo '<html><head></head><body>';
-			echo '<script language="javascript">';
-			echo 'window.location="editor.php"';
-			echo '</script>';
-			echo '</body></html>';
+		echo '<html><head></head><body>';
+		echo '<script language="javascript">';
+		echo 'window.location="editor.php"';
+		echo '</script>';
+		echo '</body></html>';
 			
-			break;
+		break;
 		/*
-		case ("2"):
+		 case ("2"):
 			//echo 'Periodista';
-			
+				
 			echo '<html><head></head><body>';
 			echo '<script language="javascript">';
 			echo 'window.location="periodista.php"';
 			echo '</script>';
 			echo '</body></html>';
-			
+				
 			break;
-		*/
-		default:
-			//echo 'ERROR El tipo de usuario NO coincide con los predeterminados';
+			*/
+	default:
+		//echo 'ERROR El tipo de usuario NO coincide con los predeterminados';
 			
-			echo '<html><head></head><body>';
-			echo '<script language="javascript">';
-			//echo 'window.location="index.php"';
-			echo 'window.location="nuevo_usuario.php"';
-			echo '</script>';
-			echo '</body></html>';
+		echo '<html><head></head><body>';
+		echo '<script language="javascript">';
+		//echo 'window.location="index.php"';
+		echo 'window.location="nuevo_usuario.php"';
+		echo '</script>';
+		echo '</body></html>';
 			
-			break;
-	}
+		break;
+}
 
 ?>
--- web_news_22_10_2012_reducido.sql
+++ web_news_22_10_2012_reducido.sql
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `newsId` int(11) NOT NULL,
-  `visitorUsername` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `visitorUsername` varchar(128) COLLATE utf8_spanish_ci DEFAULT NULL,
   `description` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
   `creationDate` datetime NOT NULL,
   PRIMARY KEY (`id`,`newsId`),
@@ -54,7 +54,7 @@ INSERT INTO `comments` (`id`, `newsId`, `visitorUsername`, `description`, `creat
 
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userUsername` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `userUsername` varchar(128) COLLATE utf8_spanish_ci NOT NULL,
   `title` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
   `description` varchar(1000) COLLATE utf8_spanish_ci NOT NULL,
   `creationDate` datetime NOT NULL,
@@ -68,18 +68,18 @@ CREATE TABLE IF NOT EXISTS `news` (
 --
 
 INSERT INTO `news` (`id`, `userUsername`, `title`, `description`, `creationDate`, `isDeleted`) VALUES
-(2, 'definitivo', 'tit', 'contenido', '2012-10-20 01:45:48', '0'),
-(3, 'son', 'test 2', 'prueba 2 con select desde bdd\r\nson', '2012-10-20 04:19:35', '0'),
-(5, 'son', 'desde', 'periodista SON', '2012-10-20 06:38:38', '0'),
-(6, 'renu', 'test', 'noticia\ncreada\ndesde\nel editor\na nombre\nde RENU', '2012-10-20 07:09:18', '0'),
-(15, 'definitivo', '11111111112222', 'sadas', '2012-10-20 14:46:31', '0'),
-(16, 'definitivo', 'El jefe de la AuditorÃ­a le apunta a De Vido por la tragedia de Once', 'asfafa', '2012-10-20 14:49:21', '0'),
-(18, 'definitivo', 'asfd', 'asfaaaaaa', '2012-10-20 15:18:21', '1'),
-(20, 'renu', 'asd', 'PresentarÃ¡n la iniciativa en los prÃ³ximos dÃ­as. La reforma apunta a que la modificaciÃ³n bonaerense estÃ© en lÃ­nea con el proyecto que el Frente Para la Victoria presentÃ³ en el Congreso de la NaciÃ³n. ', '2012-10-20 18:48:58', '0'),
-(28, 'renu', 'intento 3', 'Debo leer datos de una columna de tipo de datos image, almacenado como hexadecimal,\r\ny convertirlo a varchar, eso lo he logrado ya. El problema es que los datos que estan almacenados sobrepasan los 8000 caracteres, que es lo maximo que almacena un valor de \r\ntipo varchar en sql server 2000 a diferencia del sql server 2005, que almacena hasta 2GB de data usando el max , el problema es que en la empresa donde laboro tienen el sql server 2000, y cuando realizo la conversion solo convierte hasta 8000 y el resto no.\r\n \r\nPor eso queria saber si existe algun tipo de dato de texto que almacene mas de 8000 caracteres o si es que se pueda instalar un parche al sql 2000 para que el varchar logre almacenar mas data, o quizas poder pasar esa imformacion a un archivo de texto y luego\r\nleerlo desde alli, he visto que es utilizando el comando bcp, pero todavia estoy aprendiendo a utilizarlo.', '2012-10-20 19:10:10', '0'),
-(29, 'renu', 'sin comillas', 'Leandro Despouy dijo que el Ministerio de PlanificaciÃ³n tenÃ­a la responsabilidad primaria en la prestaciÃ³n del servicio. Y que ademÃ¡s, contaba con pruebas sobre el mal desempeÃ±o de Ricardo Jaime en Transporte.', '2012-10-20 19:12:28', '0'),
-(33, 'renu', 'Noticia de Periodista', 'momomomodmoamdoas', '2012-10-20 23:06:44', '0'),
-(35, 'renu', 'nueva nota test', 'algun texto', '2012-10-21 17:30:57', '0');
+(2, 'ddf3624547e7b62b131d431e0c3a88b4', 'tit', 'contenido', '2012-10-20 01:45:48', '0'),
+(3, '498d3c6bfa033f6dc1be4fcc3c370aa7', 'test 2', 'prueba 2 con select desde bdd\r\nson', '2012-10-20 04:19:35', '0'),
+(5, '498d3c6bfa033f6dc1be4fcc3c370aa7', 'desde', 'periodista SON', '2012-10-20 06:38:38', '0'),
+(6, 'ef88f3f374aa10d1493757bb6a4046a6', 'test', 'noticia\ncreada\ndesde\nel editor\na nombre\nde RENU', '2012-10-20 07:09:18', '0'),
+(15, 'ddf3624547e7b62b131d431e0c3a88b4', '11111111112222', 'sadas', '2012-10-20 14:46:31', '0'),
+(16, 'ddf3624547e7b62b131d431e0c3a88b4', 'El jefe de la AuditorÃ­a le apunta a De Vido por la tragedia de Once', 'asfafa', '2012-10-20 14:49:21', '0'),
+(18, 'ddf3624547e7b62b131d431e0c3a88b4', 'asfd', 'asfaaaaaa', '2012-10-20 15:18:21', '1'),
+(20, 'ef88f3f374aa10d1493757bb6a4046a6', 'asd', 'PresentarÃ¡n la iniciativa en los prÃ³ximos dÃ­as. La reforma apunta a que la modificaciÃ³n bonaerense estÃ© en lÃ­nea con el proyecto que el Frente Para la Victoria presentÃ³ en el Congreso de la NaciÃ³n. ', '2012-10-20 18:48:58', '0'),
+(28, 'ef88f3f374aa10d1493757bb6a4046a6', 'intento 3', 'Debo leer datos de una columna de tipo de datos image, almacenado como hexadecimal,\r\ny convertirlo a varchar, eso lo he logrado ya. El problema es que los datos que estan almacenados sobrepasan los 8000 caracteres, que es lo maximo que almacena un valor de \r\ntipo varchar en sql server 2000 a diferencia del sql server 2005, que almacena hasta 2GB de data usando el max , el problema es que en la empresa donde laboro tienen el sql server 2000, y cuando realizo la conversion solo convierte hasta 8000 y el resto no.\r\n \r\nPor eso queria saber si existe algun tipo de dato de texto que almacene mas de 8000 caracteres o si es que se pueda instalar un parche al sql 2000 para que el varchar logre almacenar mas data, o quizas poder pasar esa imformacion a un archivo de texto y luego\r\nleerlo desde alli, he visto que es utilizando el comando bcp, pero todavia estoy aprendiendo a utilizarlo.', '2012-10-20 19:10:10', '0'),
+(29, 'ef88f3f374aa10d1493757bb6a4046a6', 'sin comillas', 'Leandro Despouy dijo que el Ministerio de PlanificaciÃ³n tenÃ­a la responsabilidad primaria en la prestaciÃ³n del servicio. Y que ademÃ¡s, contaba con pruebas sobre el mal desempeÃ±o de Ricardo Jaime en Transporte.', '2012-10-20 19:12:28', '0'),
+(33, 'ef88f3f374aa10d1493757bb6a4046a6', 'Noticia de Periodista', 'momomomodmoamdoas', '2012-10-20 23:06:44', '0'),
+(35, 'ef88f3f374aa10d1493757bb6a4046a6', 'nueva nota test', 'algun texto', '2012-10-21 17:30:57', '0');
 
 -- --------------------------------------------------------
 
@@ -105,34 +105,39 @@ INSERT INTO `roles` (`id`, `description`) VALUES
 
 --
 -- Estructura de tabla para la tabla `users`
---
-
+-- A CONTINUACION EL NUEVO SCRIPT DE LA TABLA MODIFICADA DE USERS.
 CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `username` varchar(128) COLLATE utf8_spanish_ci NOT NULL,
   `roleId` int(11) NOT NULL,
   `dni` bigint(15) NOT NULL,
   `name` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
   `surname` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8_spanish_ci NOT NULL,
   `creationDate` datetime NOT NULL,
   `isDeleted` enum('0','1') COLLATE utf8_spanish_ci DEFAULT '0',
   PRIMARY KEY (`username`),
   KEY `roleId` (`roleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+
 --
 -- Volcado de datos para la tabla `users`
 --
 
+-- A CONTINUACION EL MISMO SCRIPT INICIAL CON LOS PASSWORDS ENCRIPTADOS
 INSERT INTO `users` (`username`, `roleId`, `dni`, `name`, `surname`, `password`, `creationDate`, `isDeleted`) VALUES
-('definitivo', 1, 2345, 'defi', 'nitivo', 'yui', '2012-10-20 01:15:04', '1'),
-('erdf', 1, 4565, 'asd', 'fffg', 'ee', '2012-10-20 03:49:30', '0'),
-('kipildor', 1, 28473674, 'Leandro', 'Quipildor', 'kipi', '2012-10-21 17:34:43', '1'),
-('Lean', 1, 258, 'pru3', 'pru3', 'eee', '2012-10-20 01:00:39', '0'),
-('qwas', 2, 234, 'dasd', 'afas', 'aaa', '2012-10-20 03:49:15', '1'),
-('renu', 2, 8989, 'Lomb', 'Bausch', '111', '2012-10-20 07:08:02', '0'),
-('son', 2, 99900, 'kil', 'kill', 'son', '2012-10-20 01:13:36', '1'),
-('yu', 1, 258, 'pru3', 'pru3', 'eee', '2012-10-20 01:09:00', '1');
+('ddf3624547e7b62b131d431e0c3a88b4', 1, 2345, 'defi', 'nitivo', '7625a98fa76ae1fdeedf46a7f5b3a58810a69d93', '2012-10-20 01:15:04', '1'),
+('170c9c6701ea5fbce9da60d0cad4b0cd', 1, 4565, 'asd', 'fffg', '56c5aa12df50651fbbcc280bbe3840c1bd0d9131', '2012-10-20 03:49:30', '0'),
+('1e9c89a31fd7d98547da4022e200bdfc', 1, 28473674, 'Leandro', 'Quipildor', '79f893df78cbc22b26e17d7ca6820be97914c190', '2012-10-21 17:34:43', '1'),
+('6050cb77ad130996b02ddecdb822b6cd', 1, 258, 'pru3', 'pru3', 'e7bf0445625ef16b79463aaa23c9e95aea554b90', '2012-10-20 01:00:39', '0'),
+('ce827a5f1df5b82656d6ecebf1cd20ab', 2, 234, 'dasd', 'afas', '90b0287495b81ae17e6605922c5245cfc5e8115c', '2012-10-20 03:49:15', '1'),
+('ef88f3f374aa10d1493757bb6a4046a6', 2, 8989, 'Lomb', 'Bausch', '87e1e9efd6047c7f41f1ac707b29de50a0ab1ea9', '2012-10-20 07:08:02', '0'),
+('498d3c6bfa033f6dc1be4fcc3c370aa7', 2, 99900, 'kil', 'kill', '77b39aa2a1104b1ecc01d79d06f59ae6d7064586', '2012-10-20 01:13:36', '1'),
+('385d04e7683a033fcc6c6654529eb7e9', 1, 258, 'pru3', 'pru3', 'e7bf0445625ef16b79463aaa23c9e95aea554b90', '2012-10-20 01:09:00', '1');
+
+
+
+
 
 --
 -- Restricciones para tablas volcadas
